f8cfe1273aa4548893aa7d83dc32d3467934fcec
ahmedessa789-lang <ahmedessa789@gmail.com>
2026-07-14 18:12:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://rkopthkajjhgyxvdmmoy.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_nCbJnPsxfuR6dXu8qFJlgw_LckabuKS",
+  APP_NAME: "Sales & P&L",
+  BRAND_NAME: "Root Eye Systems"
+};
